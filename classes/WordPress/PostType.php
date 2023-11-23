90b54f56f17774e4dff3e21173e1e1dcced01e41
Maxemilian Grönblom <maxemiliang@gmail.com>
2023-11-23 15:30:39 +0200

    feat(ydin-wordpress): Added posttype and taxonomy classes ✨

---
+++ classes/WordPress/PostType.php
@@ -0,0 +1,53 @@
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
+
+namespace Jcore\Ydin\WordPress;
+
+/**
+ * Post type specific functionality
+ *
+ * @since 3.0.0
+ * @package Jcore\Ydin\WordPress
+ */
+class PostType {
+
+	/**
+	 * Register post type wrapper.
+	 *
+	 * @param string $slug The slug of CPT..
+	 * @param array  $label_array Array of labels for CPT.
+	 * @param array  $arguments Array of custom arguments for CPT.
+	 *
+	 * @see https://developer.wordpress.org/reference/functions/register_post_type/ For further documentations for arguments.
+	 *
+	 * @return void
+	 */
+	public static function register( string $slug, array $label_array = array(), array $arguments = array() ): void {
+		$labels        = array(
+			'name'          => str_replace( '_', ' ', ucfirst( $slug ) ),
+			'singular_name' => str_replace( '_', ' ', ucfirst( $slug ) ),
+		);
+		$parsed_labels = wp_parse_args( $label_array, $labels );
+
+		$args = array(
+			'labels'              => $parsed_labels,
+			'description'         => $parsed_labels['name'],
+			'public'              => true,
+			'show_ui'             => true,
+			'delete_with_user'    => false,
+			'show_in_rest'        => true,
+			'has_archive'         => true,
+			'exclude_from_search' => false,
+			'map_meta_cap'        => true,
+			'rewrite'             => array(
+				'slug'       => $slug,
+				'with_front' => true,
+			),
+			'supports'            => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+		);
+
+		$parsed_args = wp_parse_args( $arguments, $args );
+
+		register_post_type( $slug, $parsed_args );
+	}
+
+}
---
+++ classes/WordPress/Taxonomy.php
@@ -0,0 +1,54 @@
+<?php // phpcs:ignore
+
+namespace Jcore\Ydin\WordPress;
+
+/**
+ * Taxonomy related functions.
+ *
+ * @since 3.0.0
+ * @package Jcore\Ydin\WordPress
+ */
+class Taxonomy {
+
+	/**
+	 * Register taxonomy wrapper.
+	 *
+	 * @param string $slug The taxonomy slug.
+	 * @param array  $post_types Array of post types to associate taxonomy with.
+	 * @param array  $label_array Array of labels for taxonomy.
+	 * @param array  $arguments Array of custom arguments for taxonomy.
+	 *
+	 * @see https://developer.wordpress.org/reference/functions/register_taxonomy/ For further documentations for arguments.
+	 *
+	 * @return void
+	 */
+	public static function register( string $slug, array $post_types = array( 'post', 'page' ), array $label_array = array(), array $arguments = array() ): void {
+		$labels        = array(
+			'name'          => str_replace( '_', ' ', ucfirst( $slug ) ),
+			'singular_name' => str_replace( '_', ' ', ucfirst( $slug ) ),
+		);
+		$parsed_labels = wp_parse_args( $label_array, $labels );
+
+		$rewrite     = array(
+			'slug'         => $slug,
+			'with_front'   => true,
+			'hierarchical' => true,
+		);
+		$args        = array(
+			'labels'            => $parsed_labels,
+			'hierarchical'      => true,
+			'public'            => true,
+			'show_ui'           => true,
+			'show_in_rest'      => true,
+			'show_admin_column' => true,
+			'show_in_nav_menus' => true,
+			'show_tagcloud'     => true,
+			'rewrite'           => $rewrite,
+			'query_var'         => true,
+		);
+		$parsed_args = wp_parse_args( $arguments, $args );
+
+		register_taxonomy( $slug, $post_types, $parsed_args );
+	}
+
+}
